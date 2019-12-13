0e336afc329e2225ce50c9f5fd4885482647c799
JDev129 <jchristman129@gmail.com>
2019-12-13 15:24:09 -0500

    Add files via upload

---
+++ LabMotorTest/Domain/ULMotorTestValidation/PhaseValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CertificationStandard.Domain.Helpers;
+using ConsoleInterpreter;
+
+namespace CertificationStandard.Domain.CertificationMotorTestValidation
+{
+    public class PhaseValidator : IConsoleInputValidator
+    {
+        public Func<object, bool> Validate => new MotorValidationCenter().PhaseIsValid;
+        public string InputPrompt => new MotorValidationCenter().PhaseRequest();
+        public string ValueName => "Phase";
+    }
+}
